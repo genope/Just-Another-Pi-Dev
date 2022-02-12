1c127f8290120368964747e20ebcd42ae1ec1f61
Hassen Mabrouk <hassen.mabrouk@esprit.tn>
2022-02-12 14:07:00 +0100

    Squelette Project

---
+++ src/shared/main/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shared.main;
+
+import java.sql.Date;
+import shared.connexion.MaConnexion;
+
+
+/**
+ *
+ * @author user
+ */
+public class Main {    
+    public static void main(String[] args) {
+          
+         }
+    
+}
