eb4b0d611c7922d804622ff3dbf898351209cb39
Hassen Mabrouk <hassen.mabrouk@esprit.tn>
2022-02-12 14:34:27 +0100

    changes

--- src/shared/Shared.java
+++ src/shared/Shared.java
@@ -1,22 +1,11 @@
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package shared;
 
 import shared.connexion.MaConnexion;
 
-/**
- *
- * @author user
- */
+
 public class Shared {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
         MaConnexion m = MaConnexion.getInstance();
     }
