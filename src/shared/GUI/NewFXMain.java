eadeddb07aedeefa063081e3d1fc12c013de60b6
Hassen Mabrouk <58844406+genope@users.noreply.github.com>
2022-05-17 12:57:23 +0100

    changes

--- src/shared/GUI/NewFXMain.java
+++ src/shared/GUI/NewFXMain.java
@@ -1,42 +1,43 @@
-    /*
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package shared.gui;
+package shared.GUI;
 
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 /**
  *
- * @author genop
+ * @author user
  */
-// #ffa781 : orange
-// #5b0e2d : red
 public class NewFXMain extends Application {
-    
+
     @Override
     public void start(Stage primaryStage) {
- 
+
         try {
-            Parent root =FXMLLoader.load(getClass().getResource("login_page.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("MenuHost.fxml"));
             Scene scene = new Scene(root);
             scene.getStylesheets().add(getClass().getResource("Style/controls.css").toExternalForm());
-            //primaryStage.setTitle("Hello World!");
-            //remove titlebare
            primaryStage.initStyle(StageStyle.UNDECORATED);
             primaryStage.setScene(scene);
             primaryStage.show();
         } catch (IOException ex) {
-            Logger.getLogger(NewFXMain.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println(ex.getMessage());
+
         }
     }
 
@@ -46,5 +47,5 @@ public class NewFXMain extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
